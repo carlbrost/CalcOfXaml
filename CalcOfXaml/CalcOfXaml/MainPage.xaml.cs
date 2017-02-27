6ad83c4a0156da3c16c96aec6a7820b3ed012098
carlbrost <brostmech@gmail.com>
2017-02-27 15:03:13 -0600

    I just sort of figured out how to do the github thing properly today, so I'm prettymuch just throwing them up when I have them done.

--- CalcOfXaml/CalcOfXaml/MainPage.xaml.cs
+++ CalcOfXaml/CalcOfXaml/MainPage.xaml.cs
@@ -9,9 +9,90 @@ namespace CalcOfXaml
 {
     public partial class MainPage : ContentPage
     {
+
+
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+
+        void SelectNum(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if (this.calcText.Text == "0" || currentState < 0)
+            {
+                this.calcText.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            this.calcText.Text += pressed;
+
+            double number;
+            if (double.TryParse(this.calcText.Text, out number))
+            {
+                this.calcText.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+
+        void SelectOp(object sender, EventArgs e)
+        {
+            currentState = -2;
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            mathOperator = pressed;
+        }
+
+        void SelectClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.calcText.Text = "0";
+        }
+
+        void SelectCalc(object sender, EventArgs e)
+        {
+            if (currentState == 2)
+            {
+                double result = 0;
+                switch (mathOperator)
+                {
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        break;
+                    case "X":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    default:
+                        break;
+                }
+
+                firstNumber = result;
+                this.calcText.Text = result.ToString("N0");
+                currentState = -1;
+            }
         }
     }
 }
